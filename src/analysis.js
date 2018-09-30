de4f4ee3089ee1593399092b6e1947885d494689
nilsgeilen <7262115+nilsgeilen@users.noreply.github.com>
2018-09-30 18:34:55 +0200

    Add charts

--- src/analysis.js
+++ src/analysis.js
@@ -1,7 +1,12 @@
 
 const COLOR_CNT = "blue"
 const COLOR_AVG = "red"
+const COLOR_RATING = "orange"
 const COLOR_COPROD = "gray"
+const COLOR_OTHER = "gray"
+
+const STD_COLORS = ["#ff8000", "#00ff80", "#ff0080", "#80ff00", "#0080ff", "#8000ff",
+    "#a0ff00", "#00a0ff", "#a000ff", "#ffa000", "#00ffa0", "#ff00a0"]
 
 const COPRODUCTION = "coproduction"
 
@@ -31,9 +36,7 @@ const visualizeData = function () {
         films = films.filter(film => film.title_type && film.title_type.match(/movie|tvMovie/))
         charts_async = []
         displayCoutryStatsAsync(films, charts_async)
-        charts = displayDirectorStats(films)
-            .concat(displayDecadeStats(films))
-            .concat(scatterRuntime(films))
+        charts = [...displayDirectorStats(films), ...displayDecadeStats(films), ...scatterRuntime(films)]
     }
 }()
 
@@ -56,7 +59,7 @@ function displayDirectorStats (films, N = 10) {
     let directors = createDirectorList(films)
     for (let director of directors) {
             director.film_cnt = director.films.length,
-            director.avg_rating = avg(director.films, "your_rating")
+            director.avg_rating = +avg(director.films, getter("your_rating")).toFixed(1)
     }
     let top_10 = directors.sort((a, b) => (b.film_cnt * 1000 + b.avg_rating) - (a.film_cnt * 1000 + a.avg_rating)).slice(0, N)
 
@@ -76,29 +79,55 @@ function displayDirectorStats (films, N = 10) {
 
     let top_diff = directors.filter(director => director.film_cnt >= 2).map(director => ({
         director : director, 
-        diff : argmax(director.films, "your_rating").your_rating - argmin(director.films, "your_rating").your_rating
+        diff : argmax(director.films, getter("your_rating")).your_rating - argmin(director.films, getter("your_rating")).your_rating
     })).sort((a,b) => b.diff - a.diff).slice(0,5).map(x => ({
         name: x.director.name,
         diff: x.diff,
-        films: x.director.films.slice().sort((a,b) => a.your_rating - b.your_rating)
+        avg_rating : x.director.avg_rating,
+        films: Object.entries(x.director.films.reduce((map, film) => {
+            if (film.your_rating in map)
+                map[film.your_rating].push(film)
+            else map[film.your_rating] = [film]
+            return map
+        }, {})).sort((a,b) => b[0] - a[0]).map(entry => ({
+            your_rating : entry[0],
+            title : entry[1].map(film => film.title).join(', '),
+            film_cnt : entry[1].length
+        }))
     }))
 
-    let datasets = top_diff.map(elem => ({
+    
+    let datasets = top_diff.map(elem => {
+        let sizes = elem.films.map(film => 6.0 + film.film_cnt * 3.0)
+        return {
+            //label: elem.director.name,
+            data: elem.films.map(film => ({x: film.your_rating, y: elem.name, title: film.title})),
+            fill: false,
+            borderColor: COLOR_RATING,
+            backgroundColor: COLOR_RATING,
+            pointStyle : 'rectRounded',
+            pointRadius : sizes,
+            pointHoverRadius : sizes,
+            pointHitRadius : sizes
+    }}).concat([{
         //label: elem.director.name,
-        data: elem.films.map(film => ({x: film.your_rating, y: elem.name, title: film.title})),
+        data: top_diff.map(elem =>({x: elem.avg_rating, y: elem.name, title: elem.name})),
         fill: false,
+        showLine: false,
         borderColor: COLOR_AVG,
         backgroundColor: COLOR_AVG,
-        pointStyle : 'rectRounded',
+        pointStyle : 'star',
         pointRadius : 10.0,
         pointHoverRadius :10.0,
         pointHitRadius : 10.0
-    }))
+    }])
+
+    console.log(datasets)
 
     let chart2 = new Chart($("#ctx1b"), {
         type: 'line',
         data: {
-            yLabels : [""].concat(top_diff.map(x => x.name)).concat([""]),
+            yLabels : ["", ...top_diff.map(x => x.name), ""],
             datasets : datasets
         },
         options: {
@@ -127,7 +156,43 @@ function displayDirectorStats (films, N = 10) {
         }
     })
 
-    return [chart1, chart2]
+    let datasets_chrono = zip([top_10, STD_COLORS]).map(([director, color]) => ({
+            label: director.name,
+            data: director.films.sort((a, b) => b.year - a.year).map(film => ({x: film.year, y: film.your_rating, title: film.title})),
+            fill: false,
+            borderColor: color,
+            backgroundColor: color,
+            pointStyle : 'rectRounded',
+            pointRadius : 10.0,
+            pointHoverRadius : 10.0,
+            pointHitRadius : 10.0
+    }))
+
+    let chart3 = new Chart($("#ctx1c"), {
+        type: 'line',
+        data: {
+            datasets : datasets_chrono
+        },
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'linear',
+                    position: 'bottom'
+                }],
+                yAxes: [{
+                    type: 'linear',
+                    position: 'left'
+                    }]
+            },
+            tooltips: {
+                callbacks: {
+                   label: (tooltipItem, data) => data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index].title
+                }
+             }
+        }
+    })
+
+    return [chart1, chart2, chart3]
 }
 
 function displayDecadeStats(films) {
@@ -151,7 +216,7 @@ function displayDecadeStats(films) {
 
     for (let key of Object.keys(decades).sort()) {
         dataset1.data.push(decades[key].length)
-        dataset2.data.push(avg(decades[key], "your_rating"))
+        dataset2.data.push(+avg(decades[key], getter("your_rating")).toFixed(1))
     }
 
     let chart1 = new Chart($("#ctx2"), {
@@ -188,10 +253,10 @@ function displayDecadeStats(films) {
 
 function scatterRuntime(films) {
 
-    films = films.filter(film => film.runtime_mins && film.runtime_mins.match(/\d+/) && film.title_type.match(/movie|tvMovie/))
+    films = films.filter(film => film.runtime_mins && film.runtime_mins.match(/\d+/))
 
     let dataset = {
-        backgroundColor : "red",
+        backgroundColor : COLOR_RATING,
         label: 'Scatter Dataset',
         data: []
     }
@@ -239,18 +304,24 @@ function displayCoutryStatsAsync(films, charts) {
                 continue
             }
             country.film_cnt = country.films_excl_coprod.length
-            country.avg_rating = avg(country.films, "your_rating")
+            country.avg_rating = +avg(country.films, getter("your_rating")).toFixed(1)
         }
 
         countries.sort((a,b) => a.film_cnt < b.film_cnt)
 
-        const N_1 = countries.length > 12 ? 12 : countries.length
+        let N_1 = 12
+        N_1 = countries.length > N_1 ? N_1 : countries.length
+        for (let i = 0; i < N_1; i++) {
+            if (countries[i].film_cnt === 1) {
+                N_1 = i
+                break
+            }
+        }
 
         let dataset_cnt = {
-            backgroundColor : ["#ff8000", "#00ff80", "#ff0080", "#80ff00", "#0080ff", "#8000ff",
-                "#a0ff00", "#00a0ff", "#a000ff", "#ffa000", "#00ffa0", "#ff00a0"],
+            backgroundColor : STD_COLORS.slice(0, N_1).concat([COLOR_OTHER]),
             label: '# of Films',
-            data: countries.slice(0,N_1).map(entry => entry.film_cnt).concat([sum(countries.slice(N_1), "film_cnt")])
+            data: [...countries.slice(0,N_1).map(entry => entry.film_cnt), sum(countries.slice(N_1), getter("film_cnt"))]
         }
 
         for (let i = 0; i < dataset_cnt.backgroundColor.length; i++) {
@@ -283,21 +354,21 @@ function displayCoutryStatsAsync(films, charts) {
         let dataset_excl_coprod = {
             backgroundColor : "#000040",
             label: '# of domestic films',
-            data: top_countries.map(entry => entry.film_cnt).concat([sum(countries.slice(N_2), "film_cnt")])
+            data: [...top_countries.map(entry => entry.film_cnt), sum(countries.slice(N_2), getter("film_cnt"))]
         }
 
         let dataset_coprod = {
             backgroundColor : "#0000ff",
             label: '# of coproductions',
-            data: top_countries.map(entry => entry.films.length  - entry.film_cnt).concat([sum(countries.slice(N_2), "film_cnt")])
+            data: [...top_countries.map(entry => entry.films.length  - entry.film_cnt), sum(countries.slice(N_2), getter("film_cnt"))]
         }
 
         charts.push(plotBarChart($("#ctx4b"), top_countries.map(entry => entry.name), [dataset_excl_coprod, dataset_coprod], false, true))
 
-        const N_3 = N_2/2
+        const N_3 = 8
         let countries_by_avg_rating = countries.filter(country => country.films.length >= 2).sort((a,b) => b.avg_rating - a.avg_rating)
         if (countries_by_avg_rating.length > N_3*2+1)
-            countries_by_avg_rating = countries_by_avg_rating.slice(0,N_3).concat([{name: '...', avg_rating: 1}]).concat(countries_by_avg_rating.slice(-N_3))
+            countries_by_avg_rating = [...countries_by_avg_rating.slice(0,N_3), {name: '...', avg_rating: 1}, ...countries_by_avg_rating.slice(-N_3)]
         let dataset_avg = {
             backgroundColor : COLOR_AVG,
             label: 'Average Rating',
@@ -373,6 +444,7 @@ function createCountryList(films, map) {
         if (countries) {
             countries = countries.split(/,/)
             for (let country of countries) {
+                country = country.trim()
                 if (result[country]) {
                     result[country].films.push(film)
                 } else {
@@ -389,8 +461,8 @@ function createDirectorList (data) {
     let directors = {}
     for (let film of data) {
         if (film.directors) {
-            let ds = film.directors.split(/,/)
-            for (let director of ds) {
+            for (let director of film.directors.split(/,/)) {
+                director = director.trim()
                 if (directors[director]) {
                     directors[director].films.push(film)
                 } else {
--- src/util.js
+++ src/util.js
@@ -6,52 +6,36 @@ function avg (list, field) {
 
 function sum (list, field) {
     if (field) 
-        return list.reduce((sum, val) => sum + +val[field], 0)
+        return list.reduce((sum, val) => sum + +field(val), 0)
     else return list.reduce((sum, val) => sum + +val, 0)
 }
 
 function argmax (list, field) {
     if (list.length === 0)
         return null
-    if (!field) {
-        throw "not supported yet!"
-    } else if (typeof field === 'function') {
-        let result = list[0]
-        for (let i = 1; i < list.length; i++) {
-            if (field(list[i], result) > 0)
-                result = list[i]
-        }
-        return result
-    } else if (typeof field === 'string') {
-        let result = list[0]
-        for (let i = 1; i < list.length; i++) {
-            if (+list[i][field] > +result[field])
-                result = list[i]
-        }
-        return result
-    } 
+    if (!field) 
+        field = id => id
+
+    let result = list[0]
+    for (let i = 1; i < list.length; i++) {
+        if (field(list[i], result) > 0)
+            result = list[i]
+    }
+    return result
 }
 
 function argmin (list, field) {
     if (list.length === 0)
         return null
-    if (!field) {
-        throw "not supported yet!"
-    } else if (typeof field === 'function') {
-        let result = list[0]
-        for (let i = 1; i < list.length; i++) {
-            if (field(list[i], result) < 0)
-                result = list[i]
-        }
-        return result
-    } else if (typeof field === 'string') {
-        let result = list[0]
-        for (let i = 1; i < list.length; i++) {
-            if (+list[i][field] < +result[field])
-                result = list[i]
-        }
-        return result
-    } 
+    if (!field) 
+        field = id => id
+    
+    let result = list[0]
+    for (let i = 1; i < list.length; i++) {
+        if (field(list[i], result) < 0)
+            result = list[i]
+    }
+    return result
 }
 
 function valList (map) {
@@ -59,4 +43,20 @@ function valList (map) {
     for (let i in map)
         list.push(map[i])
     return list
+}
+
+function getter (field) {
+    return obj => obj[field]
+}
+
+function zip (lists) {
+    let result = []
+    //const N = lists.reduce((min, list) => Math.min(min, list.length), lists.length ? lists[0].length : 0) 
+    const N = Math.min(... lists.map(getter('length')))
+    console.log(N)
+    for (let i = 0; i < N; i++) {
+        result.push(lists.map(list => list[i]))
+    }
+    console.log(result)
+    return result
 }
